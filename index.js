cde63b7d2ea87ac2dace0a5d2979da1938acb0ec
JosephMureithi <darkknight505c@gmail.com>
2024-09-23 21:29:57 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400 ){
+    return 'This one is on me!';
+  }else  if (ride >400 && ride<2000){
+    return 'That will be twenty bucks.';
+  }else if (ride < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else{
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ if (city == "NYC"){
+  return 'Ok, sounds good.';
+ }
+ else if (city !== 'NYC'){
+  return "No go.";
+ }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
+switch (Tip) {
+ case "generous":
+  return "Thank you so much.";
+ case "not as generous":
+  return "Thank you.";
+ default:
+  return "Bye.";
+ }
 }
